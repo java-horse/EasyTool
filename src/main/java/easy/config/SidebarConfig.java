b3a10be71da51a0104a58cf8a6582f1cb994e346
mabin <mabin2804@163.com>
2023-10-16 10:51:00 +0800

    代码优化

--- src/main/java/easy/config/SidebarConfig.java
+++ src/main/java/easy/config/SidebarConfig.java
@@ -9,16 +9,13 @@ import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.content.ContentManager;
 import easy.base.Constants;
 import easy.form.Statistics;
-import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.text.Document;
 import java.awt.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.SimpleTimeZone;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 侧边栏工具窗口配置
@@ -57,24 +54,12 @@ public class SidebarConfig implements ToolWindowFactory {
      */
     private void createStatisticsTab(ContentFactory contentFactory, ContentManager contentManager, PropertiesComponent propertiesComponent) throws Exception {
         // 天转换次数
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.UK);
-        dateFormat.setTimeZone(new SimpleTimeZone(8, "GMT"));
-        String dayKeyName = Constants.DAY_CONVERT_COUNT + dateFormat.format(new Date());
-        String dayCountValue = propertiesComponent.getValue(dayKeyName);
-        if (StringUtils.isBlank(dayCountValue)) {
-            propertiesComponent.setValue(dayKeyName, "0");
-        }
-        // 总转换次数
-        String totalCountValue = propertiesComponent.getValue(Constants.TOTAL_CONVERT_COUNT);
-        if (StringUtils.isBlank(totalCountValue)) {
-            propertiesComponent.setValue(Constants.TOTAL_CONVERT_COUNT, "0");
-        }
-
         Statistics statistics = Statistics.getInstance();
         JTextField dayTextField = statistics.getDayTextField();
         Document dayDocument = dayTextField.getDocument();
         dayDocument.remove(0, dayDocument.getLength());
-        dayDocument.insertString(0, Constants.PLUGIN_NAME + " 今日已为您自动转换中英文字符 " + propertiesComponent.getValue(dayKeyName) + " 次", null);
+        String dayKeyName = Constants.DAY_CONVERT_COUNT + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        dayDocument.insertString(0, Constants.PLUGIN_NAME + " 今日已为您自动转换中英文字符 " + propertiesComponent.getValue(dayKeyName, "0") + " 次", null);
         dayTextField.setDocument(dayDocument);
         dayTextField.setHorizontalAlignment(SwingConstants.CENTER);
         dayTextField.setFont(new Font("微软雅黑", Font.BOLD, 18));
@@ -84,7 +69,7 @@ public class SidebarConfig implements ToolWindowFactory {
         JTextField totalTextField = statistics.getTotalTextField();
         Document totalDocument = totalTextField.getDocument();
         totalDocument.remove(0, totalDocument.getLength());
-        totalDocument.insertString(0, Constants.PLUGIN_NAME + " 累计共为您自动转换中英文字符 " + propertiesComponent.getValue(Constants.TOTAL_CONVERT_COUNT) + " 次", null);
+        totalDocument.insertString(0, Constants.PLUGIN_NAME + " 累计共为您自动转换中英文字符 " + propertiesComponent.getValue(Constants.TOTAL_CONVERT_COUNT, "0") + " 次", null);
         totalTextField.setDocument(totalDocument);
         totalTextField.setHorizontalAlignment(SwingConstants.CENTER);
         totalTextField.setFont(new Font("微软雅黑", Font.BOLD, 18));
--- src/main/java/easy/handler/ConvertHandler.java
+++ src/main/java/easy/handler/ConvertHandler.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
@@ -16,9 +15,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 中英文字符转换处理器
@@ -31,7 +31,6 @@ import java.util.*;
 public class ConvertHandler implements TypedActionHandler {
     private static final Logger log = Logger.getInstance(ConvertHandler.class);
     public static Map<String, String> EN_ZH_CHAR_MAP = new HashMap<>(16);
-    private static PropertiesComponent PROPERTIES_COMPONENT = PropertiesComponent.getInstance();
     private final TypedActionHandler orignTypedActionHandler;
     private char lastChar = ' ';
 
@@ -53,9 +52,11 @@ public class ConvertHandler implements TypedActionHandler {
      **/
     public static void reload() {
         EN_ZH_CHAR_MAP.clear();
-        String[] configString = PropertiesComponent.getInstance()
-                .getValue(Constants.EASY_CHAR_KEY, Constants.DEFAULT_STRING)
-                .split("\n");
+        String defaultStr = PropertiesComponent.getInstance().getValue(Constants.EASY_CHAR_KEY, Constants.DEFAULT_STRING);
+        if (StringUtils.isBlank(defaultStr)) {
+            return;
+        }
+        String[] configString = defaultStr.split("\n");
         int length = configString.length / Constants.SPLIT_LENGTH;
         for (int i = 0; i < length; i++) {
             EN_ZH_CHAR_MAP.put(configString[Constants.SPLIT_LENGTH * i].trim(), configString[Constants.SPLIT_LENGTH * i + 1].trim());
@@ -74,42 +75,25 @@ public class ConvertHandler implements TypedActionHandler {
      **/
     @Override
     public void execute(@NotNull Editor editor, char c, @NotNull DataContext dataContext) {
-        Document document = editor.getDocument();
-        Project project = editor.getProject();
-        CaretModel caretModel = editor.getCaretModel();
-        Caret primaryCaret = caretModel.getPrimaryCaret();
-        int caretOffset = primaryCaret.getOffset();
         String cStr = String.valueOf(c);
         String enChar = EN_ZH_CHAR_MAP.get(cStr);
-        if (StringUtils.isBlank(PROPERTIES_COMPONENT.getValue(Constants.TOTAL_CONVERT_COUNT))) {
-            PROPERTIES_COMPONENT.setValue(Constants.TOTAL_CONVERT_COUNT, "0");
-        }
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.UK);
-        dateFormat.setTimeZone(new SimpleTimeZone(8, "GMT"));
-        String dayKeyName = Constants.DAY_CONVERT_COUNT + dateFormat.format(new Date());
-        String dayCountValue = PROPERTIES_COMPONENT.getValue(dayKeyName);
-        if (StringUtils.isBlank(dayCountValue)) {
-            PROPERTIES_COMPONENT.setValue(dayKeyName, "0");
-        }
         if (lastChar == Constants.PREFIX_CHAR && enChar != null) {
-            Runnable runnable = () -> {
+            Document document = editor.getDocument();
+            Project project = editor.getProject();
+            Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+            int caretOffset = primaryCaret.getOffset();
+            WriteCommandAction.runWriteCommandAction(project, () -> {
                 document.deleteString(caretOffset - 1, caretOffset);
                 document.insertString(caretOffset - 1, cStr);
                 primaryCaret.moveToOffset(caretOffset);
-            };
-            WriteCommandAction.runWriteCommandAction(project, runnable);
-            PROPERTIES_COMPONENT.setValue(Constants.TOTAL_CONVERT_COUNT, Long.toString(getTotalConvertCount() + 1));
-            PROPERTIES_COMPONENT.setValue(dayKeyName, Long.toString(getDayConvertCount() + 1));
+            });
         } else if (enChar != null) {
             orignTypedActionHandler.execute(editor, enChar.charAt(0), dataContext);
-            PROPERTIES_COMPONENT.setValue(Constants.TOTAL_CONVERT_COUNT, Long.toString(getTotalConvertCount() + 1));
-            PROPERTIES_COMPONENT.setValue(dayKeyName, Long.toString(getDayConvertCount() + 1));
+            updateStatisticsText();
         } else {
             orignTypedActionHandler.execute(editor, c, dataContext);
         }
         this.lastChar = c;
-        // 更新统计文本数据
-        updateStatisticsText();
     }
 
     /**
@@ -121,24 +105,20 @@ public class ConvertHandler implements TypedActionHandler {
      * @date 2023/5/23 10:54
      **/
     private Long getTotalConvertCount() {
-        String value = PROPERTIES_COMPONENT.getValue(Constants.TOTAL_CONVERT_COUNT);
-        return StringUtils.isBlank(value) ? 0 : Long.parseLong(value);
+        return PropertiesComponent.getInstance().getLong(Constants.TOTAL_CONVERT_COUNT, 0);
     }
 
     /**
      * 获取天转换次数
      *
      * @param
+     * @param dayKeyName
      * @return java.lang.Long
      * @author mabin
      * @date 2023/10/14 9:51
      */
-    private Long getDayConvertCount() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.UK);
-        dateFormat.setTimeZone(new SimpleTimeZone(8, "GMT"));
-        String dayKeyName = Constants.DAY_CONVERT_COUNT + dateFormat.format(new Date());
-        String dayCountValue = PROPERTIES_COMPONENT.getValue(dayKeyName);
-        return StringUtils.isBlank(dayCountValue) ? 0 : Long.parseLong(dayCountValue);
+    private Long getDayConvertCount(String dayKeyName) {
+        return PropertiesComponent.getInstance().getLong(dayKeyName, 0);
     }
 
     /**
@@ -151,11 +131,16 @@ public class ConvertHandler implements TypedActionHandler {
      */
     private void updateStatisticsText() {
         try {
+            PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
+            propertiesComponent.setValue(Constants.TOTAL_CONVERT_COUNT, Long.toString(getTotalConvertCount() + 1));
+            String dayKeyName = Constants.DAY_CONVERT_COUNT + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+            propertiesComponent.setValue(dayKeyName, Long.toString(getDayConvertCount(dayKeyName) + 1));
+
             Statistics statistics = Statistics.getInstance();
             JTextField dayTextField = statistics.getDayTextField();
             javax.swing.text.Document dayDocument = dayTextField.getDocument();
             dayDocument.remove(0, dayDocument.getLength());
-            dayDocument.insertString(0, Constants.PLUGIN_NAME + " 今日已为您自动转换中英文字符 " + getDayConvertCount() + " 次", null);
+            dayDocument.insertString(0, Constants.PLUGIN_NAME + " 今日已为您自动转换中英文字符 " + getDayConvertCount(dayKeyName) + " 次", null);
             dayTextField.setDocument(dayDocument);
 
             JTextField totalTextField = statistics.getTotalTextField();
@@ -163,7 +148,10 @@ public class ConvertHandler implements TypedActionHandler {
             totalDocument.remove(0, totalDocument.getLength());
             totalDocument.insertString(0, Constants.PLUGIN_NAME + " 累计共为您自动转换中英文字符 " + getTotalConvertCount() + " 次", null);
             totalTextField.setDocument(totalDocument);
-        } catch (BadLocationException e) {
+
+            log.warn("totalConvertCount: " + getTotalConvertCount());
+            log.warn("dayConvertCount: " + getDayConvertCount(dayKeyName));
+        } catch (Exception e) {
             log.error("更新工具栏统计选项卡文本数据异常", e);
         }
     }
