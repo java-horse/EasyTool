6639bc092da046314655deda1a61fa49c1ee3042
mabin <mabin2804@163.com>
2024-01-22 16:48:05 +0800

    v2.0.3
    排除非必要依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,9 @@ repositories {
 dependencies {
     // implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("java-sdk-core-3.2.4.jar"))))
     implementation("cn.hutool:hutool-http:5.8.16")
-    implementation("com.cronutils:cron-utils:9.2.1")
+    implementation("com.cronutils:cron-utils:9.2.1") {
+        exclude(group = "org.slf4j")
+    }
 }
 
 intellij {
