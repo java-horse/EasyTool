f4d53197f283c160938bcb9f9edf52f77373c7d2
mabin <mabin@wcansoft.com>
2024-04-25 18:15:11 +0800

    v2.0.7
    🎉【新增】新增显示插件新特性弹窗功能按钮

--- src/main/java/easy/handler/PluginForUpdateHandler.java
+++ src/main/java/easy/handler/PluginForUpdateHandler.java
@@ -1,14 +1,18 @@
 package easy.handler;
 
+import com.google.common.collect.Lists;
+import com.google.gson.JsonObject;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationAction;
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import easy.base.Constants;
+import easy.ui.CommonNotifyDialog;
 import easy.util.EasyCommonUtil;
 import easy.util.HttpUtil;
 import easy.util.JsonUtil;
@@ -16,6 +20,8 @@ import easy.util.NotificationUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -50,7 +56,9 @@ public class PluginForUpdateHandler {
         if (StringUtils.isBlank(response)) {
             return;
         }
-        String remoteVersion = JsonUtil.fromArray(response).get(0).getAsJsonObject().get("version").getAsString();
+        JsonObject pluginObject = JsonUtil.fromArray(response).get(0).getAsJsonObject();
+        String remoteVersion = pluginObject.get("version").getAsString();
+        String notes = pluginObject.get("notes").getAsString();
         // 比对版本信息
         if (StringUtils.equals(version, remoteVersion)) {
             NotificationUtil.notify(String.format("🎉🎉🎉 %s 已是最新版：v%s 🎉🎉🎉", Constants.PLUGIN_NAME, remoteVersion), new NotificationAction("👍 五星好评") {
@@ -58,7 +66,7 @@ public class PluginForUpdateHandler {
                 public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
                     EasyCommonUtil.confirmOpenLink(Constants.JETBRAINS_URL);
                 }
-            });
+            }, genNoteAction(notes, remoteVersion));
             return;
         }
         // 发送插件最新版本通知
@@ -67,8 +75,49 @@ public class PluginForUpdateHandler {
             public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
                 ShowSettingsUtil.getInstance().showSettingsDialog(project, "Plugins");
             }
-        });
+        }, genNoteAction(notes, remoteVersion));
 
+    }
+
+    /**
+     * 解析插件注释
+     *
+     * @param notes   注释
+     * @param version 版本
+     * @return {@link java.lang.String}
+     * @author mabin
+     * @date 2024/04/25 16:34
+     */
+    private static String parsePluginNotes(String notes, String version) {
+        if (StringUtils.isBlank(notes)) {
+            return StringUtils.EMPTY;
+        }
+        List<String> noteList = Lists.newLinkedList();
+        noteList.add("<ul>");
+        noteList.addAll(Arrays.stream(notes.split("<h3>")[1]
+                        .split("<li>-------------------------------</li>")[1]
+                        .split(StringUtils.LF))
+                .filter(StringUtils::isNotBlank)
+                .map(StringUtils::trim).toList());
+        return String.format("<h3>v%s</h3>%s", version, String.join(StringUtils.LF, noteList));
+    }
+
+    /**
+     * 显示新特性弹窗操作
+     *
+     * @param notes         注释
+     * @param remoteVersion 远程版本
+     * @return {@link com.intellij.openapi.actionSystem.AnAction}
+     * @author mabin
+     * @date 2024/04/25 17:41
+     */
+    private static AnAction genNoteAction(String notes, String remoteVersion) {
+        return new NotificationAction("🎊 升级特性") {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent anActionEvent, @NotNull Notification notification) {
+                new CommonNotifyDialog(Constants.PLUGIN_NAME, parsePluginNotes(notes, remoteVersion)).show();
+            }
+        };
     }
 
 }
