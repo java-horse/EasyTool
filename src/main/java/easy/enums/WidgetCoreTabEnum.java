0719a13a24f586774c7c07011c76396da0294307
mabin <mabin@wcansoft.com>
2024-07-23 16:29:27 +0800

    v2.1.6
    扩展ID生成小组件支持更多类型ID生成

--- src/main/java/easy/enums/WidgetCoreTabEnum.java
+++ src/main/java/easy/enums/WidgetCoreTabEnum.java
@@ -52,7 +52,7 @@ public enum WidgetCoreTabEnum {
             return new Base64CoreView().getContent();
         }
     },
-    CALCULATOR("Simple计算器") {
+    CALCULATOR("Simple计算") {
         @Override
         public Component getComponent() {
             return new CalculatorCoreView().getContent();
@@ -64,10 +64,10 @@ public enum WidgetCoreTabEnum {
             return new ProcessCoreView().getContent();
         }
     },
-    UUID("UUID生成") {
+    UUID("ID生成") {
         @Override
         public Component getComponent() {
-            return new UuidCoreView().getContent();
+            return new IdCoreView().getContent();
         }
     },
     ;
